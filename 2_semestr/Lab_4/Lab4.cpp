631520849e89e8cd935e274db1f0a50e0ed48838
C0D3RG0D <codergod@mail.ru>
2024-04-30 14:53:51 +0200

    The 8th laboratory was completed

--- 2_semestr/Lab_4/Lab4.cpp
+++ 2_semestr/Lab_4/Lab4.cpp
@@ -10,11 +10,17 @@ int main() {
 	std::cout << "¬ведите число num1: " << std::endl;
 	std::cin >> num1;
 
+	std::cout << std::endl;
+
 	std::cout << "¬ведите число num2: " << std::endl;
 	std::cin >> num2;
 
+	std::cout << std::endl;
+
 	std::cout << "¬ведите число num3: " << std::endl;
 	std::cin >> num3;
+
+	std::cout << std::endl;
 
 	my_class::BigInt num4(num1);
 
@@ -27,14 +33,22 @@ int main() {
 
 	std::cout << "num2 += num1: " << num2 << std::endl;
 
+	std::cout << std::endl;
+
 	std::cout << "num1 + num2: " << sum1 << std::endl;
+
+	std::cout << std::endl;
 	std::cout << "num1 + num3: " << sum2 << std::endl;
+
+	std::cout << std::endl;
 
 	my_class::BigInt product;
 
 	product = num1 * num2;
 
 	std::cout << "num1 * num2: " << product << std::endl;
+
+	std::cout << std::endl;
 
 	std::cout << "num1 < num4: " << (num1 < num4) << std::endl;
 	std::cout << "num1 > num4: " << (num1 > num4) << std::endl;
